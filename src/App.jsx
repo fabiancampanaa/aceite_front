3d7d8ea3ad002b8edb833054ac7bc8cf11b3f076
Fabian Campana <f.campana@live.com>
2025-02-01 20:11:19 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bulma/css/bulma.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h5>TRINIDAD</h5>
+      </div>
+
+    </>
+  )
+}
+
+export default App
