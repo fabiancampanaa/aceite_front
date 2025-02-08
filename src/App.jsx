fa5279260ad50d34abdcf9366460f4cf3f281683
Fabian Campana <38710826+fabiancampanaa@users.noreply.github.com>
2025-02-08 10:01:38 -0300

    Pagina inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import 'bulma/css/bulma.min.css'
-import { Inicio } from './components/inicio';
+import Inicio from './components/inicio';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-        <Inicio name="trini" />
+        <Inicio />
     </>
   )
 }
--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -1,5 +1,110 @@
-export function Inicio(props) {
+import React, { useRef, useState } from "react";
+import "bulma/css/bulma.min.css";
 
-    return <h5>Hola {props.name}</h5>;
-  }
+function Inicio() {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const confirmPasswordRef = useRef(null);
+  const [isRegistering, setIsRegistering] = useState(false);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (isRegistering) {
+      if (passwordRef.current.value !== confirmPasswordRef.current.value) {
+        alert("Password no coincide");
+        return;
+      }
+      console.log(
+        "Registrando usuario:",
+        "Nombre:", nameRef.current.value,
+        "Email:", emailRef.current.value,
+        "Password:", passwordRef.current.value
+      );
+    } else {
+      console.log("Logging in User:", emailRef.current.value, "Password:", passwordRef.current.value);
+    }
+  };
+
+
+
+  return (
+    <div className="hero is-flex is-align-items-center">
+      <div className="container is-flex is-justify-content-center">
+        <div className="box" style={{ width: "400px" }}>
+          <h1 className="title has-text-centered">{isRegistering ? "Registro" : "Inicio sesión"}</h1>
+          <form onSubmit={handleSubmit}>
+            {isRegistering && (
+              <div className="field">
+                <label className="label">Nombre completo</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    placeholder="e.g. John Doe"
+                    ref={nameRef}
+                    required
+                  />
+                </div>
+              </div>
+            )}
+
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="email"
+                  placeholder="ejemplo usuario@example.com"
+                  ref={emailRef}
+                  required
+                />
+              </div>
+            </div>
+
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="password"
+                  placeholder="********"
+                  ref={passwordRef}
+                  required
+                />
+              </div>
+            </div>
+
+            {isRegistering && (
+              <div className="field">
+                <label className="label">Confirme Password</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="password"
+                    placeholder="********"
+                    ref={confirmPasswordRef}
+                    required
+                  />
+                </div>
+              </div>
+            )}
+
+            <div className="field">
+              <button className="button is-primary is-fullwidth" type="submit">
+                {isRegistering ? "Registro" : "Inicio"}
+              </button>
+            </div>
+          </form>
+          <div className="has-text-centered">
+            <button className="button is-text" onClick={() => setIsRegistering(!isRegistering)}>
+              {isRegistering ? "Ya tiene una cuenta?, inicie sesión" : "No esta registrado? Registrese"}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Inicio
