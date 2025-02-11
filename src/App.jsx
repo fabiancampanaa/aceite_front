ff1ca019ddd8df20b79bb40c843f3484cd0f543d
Fabian Campana <38710826+fabiancampanaa@users.noreply.github.com>
2025-02-10 22:19:47 -0300

    registro usuario
    registro usuario

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,15 @@ import './App.css'
 import 'bulma/css/bulma.min.css'
 import Inicio from './components/inicio';
 
-
+import cors from "cors";
+const corsOrigin ={
+    origin:'http://localhost:3000', //or whatever port your frontend is using
+    credentials:true,            
+    optionSuccessStatus:200
+}
+App.use(cors(corsOrigin));
 function App() {
+  
   return (
     <>
         <Inicio />
--- src/components/inicio.jsx
+++ src/components/inicio.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import axios from "axios";
 import "bulma/css/bulma.min.css";
 
 function Inicio() {
@@ -7,103 +8,120 @@ function Inicio() {
   const passwordRef = useRef(null);
   const confirmPasswordRef = useRef(null);
   const [isRegistering, setIsRegistering] = useState(false);
+  const [error, setError] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    setError("");
+
     if (isRegistering) {
       if (passwordRef.current.value !== confirmPasswordRef.current.value) {
-        alert("Password no coincide");
+        setError("Constraseñas no coinciden");
         return;
       }
-      console.log(
-        "Registrando usuario:",
-        "Nombre:", nameRef.current.value,
-        "Email:", emailRef.current.value,
-        "Password:", passwordRef.current.value
-      );
+      
+      try {
+        
+        const datos_usuario = { username : nameRef.current.value, email : emailRef.current.value, password : passwordRef.current.value }
+        const datos_envio = {username: nameRef.current.value, email: emailRef.current.value, password:  passwordRef.current.value}
+        //const datos_envio = JSON.stringify(datos_usuario)
+        console.log(datos_envio)
+        const response = await axios.post("http://localhost:8000/api/register/", datos_envio);
+        console.log("Usuario registrado:", response.data);
+      } catch (err) {
+        setError("Fallo el registro. Por favor intente nuevamente.");
+      }
     } else {
-      console.log("Logging in User:", emailRef.current.value, "Password:", passwordRef.current.value);
+      try {
+        const response = await axios.post("http://localhost:8000/api/login/", {
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+        });
+        console.log("Usuario Logeado:", response.data);
+      } catch (err) {
+        setError("Error al iniciar sesion. Porfavor revise sus credenciales.");
+      }
     }
   };
 
 
-
   return (
-    <div className="hero is-flex is-align-items-center">
-      <div className="container is-flex is-justify-content-center">
-        <div className="box" style={{ width: "400px" }}>
-          <h1 className="title has-text-centered">{isRegistering ? "Registro" : "Inicio sesión"}</h1>
-          <form onSubmit={handleSubmit}>
-            {isRegistering && (
-              <div className="field">
-                <label className="label">Nombre completo</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    placeholder="e.g. John Doe"
-                    ref={nameRef}
-                    required
-                  />
-                </div>
-              </div>
-            )}
-
+    <div className="hero is-fullheight is-flex is-align-items-center">
+    <div className="container is-flex is-justify-content-center">
+      <div className="box" style={{ width: "400px" }}>
+        <h1 className="title has-text-centered">{isRegistering ? "Register" : "Login"}</h1>
+        {error && <p className="has-text-danger has-text-centered">{error}</p>}
+        <form onSubmit={handleSubmit}>
+          {isRegistering && (
             <div className="field">
-              <label className="label">Email</label>
+              <label className="label">Nombre</label>
               <div className="control">
                 <input
                   className="input"
-                  type="email"
-                  placeholder="ejemplo usuario@example.com"
-                  ref={emailRef}
+                  type="text"
+                  placeholder="ejmplo: Juan Perez"
+                  ref={nameRef}
                   required
                 />
               </div>
             </div>
+          )}
 
+          <div className="field">
+            <label className="label">Email</label>
+            <div className="control">
+              <input
+                className="input"
+                type="email"
+                placeholder="Ejemplo usuario@ejemplo.com"
+                ref={emailRef}
+                required
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label">Password</label>
+            <div className="control">
+              <input
+                className="input"
+                type="password"
+                placeholder="********"
+                ref={passwordRef}
+                required
+              />
+            </div>
+          </div>
+
+          {isRegistering && (
             <div className="field">
-              <label className="label">Password</label>
+              <label className="label">Confirme Contraseña</label>
               <div className="control">
                 <input
                   className="input"
                   type="password"
                   placeholder="********"
-                  ref={passwordRef}
+                  ref={confirmPasswordRef}
                   required
                 />
               </div>
             </div>
+          )}
 
-            {isRegistering && (
-              <div className="field">
-                <label className="label">Confirme Password</label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="password"
-                    placeholder="********"
-                    ref={confirmPasswordRef}
-                    required
-                  />
-                </div>
-              </div>
-            )}
-
-            <div className="field">
-              <button className="button is-primary is-fullwidth" type="submit">
-                {isRegistering ? "Registro" : "Inicio"}
-              </button>
-            </div>
-          </form>
-          <div className="has-text-centered">
-            <button className="button is-text" onClick={() => setIsRegistering(!isRegistering)}>
-              {isRegistering ? "Ya tiene una cuenta?, inicie sesión" : "No esta registrado? Registrese"}
+          <div className="field">
+            <button className="button is-primary is-fullwidth" type="submit">
+              {isRegistering ? "Registro" : "Inicio"}
             </button>
           </div>
+        </form>
+        <div className="has-text-centered">
+          <button className="button is-text" onClick={() => setIsRegistering(!isRegistering)}>
+            {isRegistering ? "Tiene cuenta? Ingrese" : "No tiene Cuenta? Registrese"}
+          </button>
         </div>
       </div>
     </div>
+  </div>
   );
 };
 
