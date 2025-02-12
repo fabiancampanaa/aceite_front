89ed553f313f63b4cc6975d2362fb16cd455aa91
Fabian Campana <38710826+fabiancampanaa@users.noreply.github.com>
2025-02-11 22:12:11 -0300

    ajuste
    ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,7 @@ import './App.css'
 import 'bulma/css/bulma.min.css'
 import Inicio from './components/inicio';
 
-import cors from "cors";
-const corsOrigin ={
-    origin:'http://localhost:3000', //or whatever port your frontend is using
-    credentials:true,            
-    optionSuccessStatus:200
-}
-App.use(cors(corsOrigin));
+
 function App() {
   
   return (
